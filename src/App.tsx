154690d448b2e04d6bdce74a06afa3976c9c3ea1
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-06-17 12:08:43 -0700

    Brought in react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
       <h1>React</h1>
@@ -9,3 +9,5 @@ function App() {
 }
 
 export default App;
+
+
