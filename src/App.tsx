1fa8b8da110c9b15cfc8bc5515d54d435b8e6e49
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-06-17 16:41:24 -0700

    Developed logic to add to cart

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,13 @@ const App = () => {
   const handleAddToCart = (clickedItem: CartItemType) => {
     setCartItems(prev => {
       // 1. Is the item already in cart? 
-      
+      const isItemInCart = prev.find(item => item.id === clickedItem.id)
+      if (isItemInCart) {
+        return prev.map(item => 
+          item.id === clickedItem.id ? { ...item, amount: item.amount + 1} : item
+        )
+      }
+      return [...prev, {...clickedItem, amount: 1}] 
     })
   };
 
