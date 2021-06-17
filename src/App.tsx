4f2b951197f91a321c57d0040788c522c303e650
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-06-17 13:09:01 -0700

    Developed grid, the way items displayed, styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 // Components
+import Item from './Item';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
@@ -8,6 +9,7 @@ import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 // Styles
 import { Wrapper } from './App.styles';
+
 // Types
 export type CartItemType = {
   id: number,
@@ -44,11 +46,17 @@ const App = () => {
 
 
   return (
-    <div className="App">
-      <h1>React</h1>
-    </div>
-  );
-}
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  )
+};
 
 export default App;
 
